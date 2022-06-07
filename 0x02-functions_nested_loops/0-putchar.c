8b31f29ee86cb5ff4a3372f3ffe51e5d27899e5b
jemila ibrahim <jemiladotto@gmail.com>
2022-06-07 05:28:54 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
