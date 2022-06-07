0cbbcf4346bf000c0326957cb17c53b8e761b127
jemila ibrahim <jemiladotto@gmail.com>
2022-06-07 05:41:09 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
  * print_alphabet - Check description
  * Description: It prints the alphabet in lowercase fallowed by a new line
@@ -9,6 +9,9 @@ void print_alphabet(void)
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 	_putchar('\n');
+	return (0);
 }
