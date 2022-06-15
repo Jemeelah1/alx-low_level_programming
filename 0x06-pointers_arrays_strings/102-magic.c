2e4385ab140dc0b03e2275c3a029c5f003e6cf54
jemila ibrahim <jemiladotto@gmail.com>
2022-06-15 13:01:59 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - check the code
  * Return: Always 0.
@@ -23,5 +22,6 @@ int main(void)
 	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
