f5f410b21414479ae3e1fb427d62cac3033fef85
jemila ibrahim <jemiladotto@gmail.com>
2022-06-20 07:40:35 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,6 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	str1 = s;
 	str2 = accept;
+
 	i = 0;
 	while (str1[i] != '\0') /*Declaring WHILE *s */
 	{
@@ -24,13 +25,16 @@ unsigned int _strspn(char *s, char *accept)
 				count++; /*count number*/
 				break;
 			}
+
 			j++; /*add j+1*/
 		}
 		if (s[i] != accept[j]) /*If aren't equals*/
 		{
 			break;
 		}
+
 		i++; /*add x+1*/
 	}
+
 	return (count); /*return the value of count*/
 }
