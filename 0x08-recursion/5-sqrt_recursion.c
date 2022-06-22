5a7f7789738024fd555820827161cb73702edbda
jemila ibrahim <jemiladotto@gmail.com>
2022-06-22 10:39:00 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,39 @@
 #include "main.h"
 /**
- * helper - Finds the square root of two numbers
- * @a: The number
- * @b: The number to test for the square root of @a
- * Return: square root
+ * _evaluate - evaluate function sqrt
+ * @i: interger
+ * @n: interger
+ * Return: evaluate sqrt
  */
-int helper(int a, int b)
+int _evaluate(int i, int n)
 {
-if (b * b > a)
-return (-1);
+	/*Evaluate function*/
+	if (n == 0 || n == 1)
+		return (n);
 
-else if (b * b == a) 
-return (b);
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
 
-else
-return (helper(a, b + 1));
+	else if (i * i == n) /*Condiction base*/
+		return (i);
 
-return (1);
-}
+	return (-1);
+
+	return (-1);
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to return the square root of
- * Return: the square root of @n
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: Sgrt_recursion
  */
 int _sqrt_recursion(int n)
 {
-if (n == 0)
-return (0);
-return (helper(n, 1));
+	int i = 0;
+
+	if (i < 0) /*If n is negative*/
+		return (-1);
+
+	else
+	{
+		return (_evaluate(i, n)); /*Recursive call*/
+	}
 }
