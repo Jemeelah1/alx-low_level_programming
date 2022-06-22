0c4b613d56c0451076183cacdc68afadceb3cbea
jemila ibrahim <jemiladotto@gmail.com>
2022-06-22 10:54:41 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,31 @@
 #include "main.h"
 /**
- * primeFinder - searches for if a number is prime
- * @a: the number to check
- * @b: the numbers we'll go through
- * Return: Whether or not the number is a prime number
+ * helper - Finds the square root of two numbers
+ * @a: The number
+ * @b: The number to test for the square root of @a
+ * Return: square root
  */
-int primeFinder(int a, int b)
+int helper(int a, int b)
 {
-	if (a <= 1 || a % b == 0)
-		return (0);
-	else if (a == b)
-		return (1);
-	else if (a > b)
-		primeFinder(a, b + 1);
+	if (b * b > a)
+		return (-1);
+
+	else if (b * b == a)
+		return (b);
+
+	else
+		return (helper(a, b + 1));
 
 	return (1);
 }
 /**
- * is_prime_number - tells us if an integer is a prime number or not
- * @n: the number to check
- * Return: 0 if the number is not prime, and 1 if it is
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to return the square root of
+ * Return: the square root of @n
  */
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 {
-	return (primeFinder(n, 2));
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
 }
