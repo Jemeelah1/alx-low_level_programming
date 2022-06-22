d2cc4275ce2167f37b55274d90a400a30088f756
jemila ibrahim <jemiladotto@gmail.com>
2022-06-22 10:47:24 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,27 @@
 #include "main.h"
 /**
- * _evaluate - evaluate function sqrt
- * @i: interger
- * @n: interger
- * Return: evaluate sqrt
+ * primeFinder - searches for if a number is prime
+ * @a: the number to check
+ * @b: the numbers we'll go through
+ * Return: Whether or not the number is a prime number
  */
-int _evaluate(int i, int n)
+int primeFinder(int a, int b)
 {
-	/*Evaluate function*/
-	if (n == 0 || n == 1)
-		return (n);
+	if (a <= 1 || a % b == 0)
+		return (0);
+	else if (a == b)
+		return (1);
+	else if (a > b)
+		primeFinder(a, b + 1);
 
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-
-	else if (i * i == n) /*Condiction base*/
-		return (i);
-
-	return (-1);
-
-	return (-1);
+	return (1);
+}
 /**
- * _sqrt_recursion - evaluate sqrt
- * @n: interger
- * Return: Sgrt_recursion
+ * is_prime_number - tells us if an integer is a prime number or not
+ * @n: the number to check
+ * Return: 0 if the number is not prime, and 1 if it is
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-	int i = 0;
-
-	if (i < 0) /*If n is negative*/
-		return (-1);
-
-	else
-	{
-		return (_evaluate(i, n)); /*Recursive call*/
-	}
+	return (primeFinder(n, 2));
 }
