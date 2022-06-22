f4f0298e3c4d29f748ea58b48dee699c3b418c93
jemila ibrahim <jemiladotto@gmail.com>
2022-06-22 09:06:26 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,15 +7,12 @@
 int factorial(int n)
 {
 	if (n == 0) /*Base condition*/
-	{
-		return (1);
-	}
+
+	return (1);
+	
 	else
-	{
-		return (-1);
-	}
+	return (-1);
 	else
-	{
-		return (n * factorial(n - 1)); /*Recursive call*/
-	}
+	return (n * factorial(n - 1)); /*Recursive call*/
+	
 }
