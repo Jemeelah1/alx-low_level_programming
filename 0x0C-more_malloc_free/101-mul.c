291931d1564b5b7cd2271c8159e556c5eb42a249
jemila ibrahim <jemiladotto@gmail.com>
2022-06-30 08:55:51 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -130,15 +130,14 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	for (len1 = 0; argv[1][len1]; len1++)
-		;
+	;
 	for (len2 = 0; argv[2][len2]; len2++)
-		;
+	;
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 	if (tabres == NULL)
 	{
 		free(tabres);
-
 		return (0);
 	}
 	for (i = len2 - 1, c = 0; i >= 0; i--)
